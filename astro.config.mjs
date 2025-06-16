5107d289beee0733d7a0c846435e8722917bf1ba
jovanialferez <vojalf@gmail.com>
2025-06-16 10:23:20 +0800

    fix: update url and readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hooglyai.github.io',
+    site: 'https://hoogly.github.io',
 });
