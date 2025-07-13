b6c1aabccc745dee7939d89b5e7c08cfb409df76
jovanialferez <vojalf@gmail.com>
2025-07-13 22:37:57 +0800

    fix: left align venn message

--- src/components/venn/components/TheirMessage.vue
+++ src/components/venn/components/TheirMessage.vue
@@ -14,7 +14,7 @@ defineProps<Props>()
     <VennAvatar />
     <div
       class="bg-white flex items-center justify-center rounded-tl-sm rounded-bl-2xl rounded-br-2xl rounded-tr-2xl p-4 max-w-md">
-      <div class="message-content">{{ message.text }}</div>
+      <div class="text-left">{{ message.text }}</div>
     </div>
   </div>
 </template>
