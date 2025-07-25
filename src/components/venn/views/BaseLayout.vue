e9cdc86f4ecd10db755dedfaa01da86023659c8b
jovanialferez <vojalf@gmail.com>
2025-07-25 15:59:25 +0800

    remove extra padding

--- src/components/venn/views/BaseLayout.vue
+++ src/components/venn/views/BaseLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-3 h-full flex flex-col container mx-auto max-w-6xl">
+  <div class="h-full flex flex-col container mx-auto max-w-6xl">
     <slot />
   </div>
 </template>
