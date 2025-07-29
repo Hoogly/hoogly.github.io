b75ea4ab6bbc1c02b76740c1d4ba5d96b79480ea
jovanialferez <vojalf@gmail.com>
2025-07-29 13:42:23 +0800

    fix: replace apple app site with using astro page

---
+++ src/pages/.well-known/apple-app-site-association.ts
@@ -0,0 +1,26 @@
+export async function GET() {
+  const appleAppSiteAssociation = {
+    "applinks": {
+      "apps": [],
+      "details": [
+        {
+          "appID": "C742WYA3LH.com.hoogly.dev",
+          "paths": [
+            "NOT /_/*",
+            "/linktest/*",
+            "/settings",
+            "/*"
+          ]
+        }
+      ]
+    }
+  };
+
+  return new Response(JSON.stringify(appleAppSiteAssociation, null, 2), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "public, max-age=3600"
+    }
+  });
+} 
